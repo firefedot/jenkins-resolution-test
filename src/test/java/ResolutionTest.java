44d8df51ffb0bdffe9fa83e0fbf2d1cfde7e6f11
kovylina <kovylina@haulmont.com>
2019-10-08 14:23:37 +0400

    Use Dimension instead of browserSize

--- src/test/java/ResolutionTest.java
+++ src/test/java/ResolutionTest.java
@@ -1,8 +1,9 @@
 import com.codeborne.selenide.Configuration;
 import org.junit.Test;
+import org.openqa.selenium.Dimension;
 
 import static com.codeborne.selenide.Selenide.open;
-import static com.codeborne.selenide.WebDriverRunner.getSelenideDriver;
+import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
 
 /**
  * Created by kovylina on 08.10.2019.
@@ -13,7 +14,8 @@ public class ResolutionTest {
     public void canOpenBrowserWithDecentResolution() {
         Configuration.browserSize = "1366x768";
         open();
-        System.out.println(getSelenideDriver().config().browserSize());
-        assert getSelenideDriver().config().browserSize().equals("1366x768");
+        Dimension s = getWebDriver().manage().window().getSize();
+        System.out.println(s);
+        assert s.equals(new Dimension(1366, 768));
     }
 }
