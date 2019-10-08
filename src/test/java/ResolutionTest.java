44fe9d1031dc73f1e5aa06903557b754817b5dbd
kovylina <kovylina@haulmont.com>
2019-10-08 14:30:19 +0400

    use assertEquals instead of assert

--- src/test/java/ResolutionTest.java
+++ src/test/java/ResolutionTest.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.Dimension;
 
 import static com.codeborne.selenide.Selenide.open;
 import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
+import static org.junit.Assert.assertEquals;
 
 /**
  * Created by kovylina on 08.10.2019.
@@ -14,8 +15,6 @@ public class ResolutionTest {
     public void canOpenBrowserWithDecentResolution() {
         Configuration.browserSize = "1366x768";
         open();
-        Dimension s = getWebDriver().manage().window().getSize();
-        System.out.println(s);
-        assert s.equals(new Dimension(1366, 768));
+        assertEquals(new Dimension(1366, 768), getWebDriver().manage().window().getSize());
     }
 }
